e4dcd7284c177ca32240ce57fed215844176c388
Volodymyr_Strilchuk <woha@ukr.net>
2025-01-23 18:52:35 +0200

    hw12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-export const limit = 10;
+export const limit = 15;
 
 export async function searchImages(query, page) {
   const API_KEY = '48224308-350e5a69bb04eb56206fb25fd';
